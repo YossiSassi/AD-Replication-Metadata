68cee147c88a0b7de484415d494b5eccf28cc3b5
Y1nTh35h3ll <yossis@protonmail.com>
2021-07-21 15:58:02 +0300

    added support for computer accounts & deleted objects

--- Replication Attribute Metadata history.ps1
+++ Replication Attribute Metadata history.ps1
@@ -1,6 +1,7 @@
+# Track past changes on your AD objects, even if event logs were wiped (e.g. during an Incident Response), using Replication metadata history. No special permissions needed (no admin required).
 # comments to yossis@protonmail.com
 
-#$Objects = "administrator", "yossis"
+#$Objects = "administrator", "yossis", "DC01$"
 $Objects = @();
 
 while ($x=1)
@@ -18,7 +19,7 @@ $ReplMetadata = @();
 $Objects | foreach {
     $user = $_; 
     $ReplMetadata += $DCs | foreach { 
-        Get-ADReplicationAttributeMetadata -Object $((Get-ADUser $user).distinguishedname) -ShowAllLinkedValues -Server $_ 
+        Get-ADReplicationAttributeMetadata -Object $((Get-ADObject -Filter {samaccountname -eq $user} -IncludeDeletedObjects).distinguishedname) -ShowAllLinkedValues -Server $_ 
         }
     }
 
