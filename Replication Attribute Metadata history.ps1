d6408870e3614c66ffa00fbc030cf688b8331c3b
1nTh35h3ll <yossis@protonmail.com>
2025-01-23 17:29:35 +0200

    Added support for well-known NT AUTHORITY SIDs (Security Identifiers) in the domain, e.g. 'Authenticated Users'

--- Replication Attribute Metadata history.ps1
+++ Replication Attribute Metadata history.ps1
@@ -1,27 +1,28 @@
-# Get past changes on specific AD accounts (users, computers) and Groups (online DC *only*), even if event logs were wiped (e.g. during an Incident Response), using Replication metadata history. No special permissions needed for Live AD query (no admin required), unless when using Offline DB (needed to port bind for LDAP queries of loaded DB in memory).
+# Track previous changes on specific AD accounts (users, computers - online & offline DB backup) and Groups (online DC only), even if event logs were wiped/not collected (e.g. during an Incident Response), using Replication metadata history. No special permissions needed for Live AD query (no admin required). When using Offline DB, local admin needed for port bind to LDAP queries of loaded DB in memory.
 # Requires ActiveDirectory Module *ONLY* when querying a live Domain Controller <in order to get attribute value(!)>
-# IMPORTANT NOTE: If a text file named .\accountnames.txt exists in the same directory with the script, it will read the samaccountname list from that file. if not, you will be prompted to type the name(s) of users or computers.
+# IMPORTANT NOTE: If a text file named .\accountnames.txt exists in the same directory with the script, it will read the samaccountname list from that file. if not, you will be prompted to type the name(s) of users, computers, groups or well-known SIDs.
 #
 # comments to: yossis@protonmail.com (1nTh35h311)
 #
-# Version: 1.2
+# Version: 1.3
 # Change Log:
-# v1.2 - Added support for group objects, for online mode only
-# v1.1a - Added practical examples on how to use this script in the field, by creating custom accountnames.txt file(s)
-# v1.1 - BadPasswordTime updated in LastOriginatingChangeTime + fixed out-gridview display bug in offline operations for attributevalue
-# v1.0.9 - minor update to LastChangeTime for LastLogon
-# v1.0.8 - added non-replicated attributes to OFFLINE operations (LogonCount, Lastlogon, BadPasswordCount & BadpasswordTime)
-# v1.0.7 - added non-replicated attributes to LIVE Domain query - LogonCount, Lastlogon, BadPasswordCount & BadpasswordTime
-# v1.0.6 - added csv output + better display of LAPS password expiration & serviceprincipalname
-# v1.0.5 - added capability to read samaccountname list from text file (instead of typing one by one into the prompt)
-# v1.0.4 - Minor improvements in sorting countable properties & dates
-# v1.0.3 - Added better parsing for the AccountExpires attribute
-# v1.0.2 - Added multi-Domain support, and check for AD module for live domain query.
-# v1.0.1 - Added offline DB support (Updated for OSDFCon 2021 "I know what your AD did last summer!.." talk)
+- v1.3 - Added support for well-known NT AUTHORITY SIDs (Security Identifiers) in the domain, e.g. 'Authenticated Users', for Online mode ONLY * (Read comment on Well-Known SIDs -->)
+- v1.2 - Added support for group objects, for Online mode ONLY
+- v1.1a - Added practical examples on how to use this script in the field, by creating custom accountnames.txt file(s)
+- v1.1 - BadPasswordTime updated in LastOriginatingChangeTime + fixed out-gridview display bug in offline operations for attributevalue
+- v1.0.9 - minor update to LastChangeTime for LastLogon
+- v1.0.8 - added non-replicated attributes to OFFLINE operations (LogonCount, Lastlogon, BadPasswordCount & BadpasswordTime)
+- v1.0.7 - added non-replicated attributes to LIVE Domain query - LogonCount, Lastlogon, BadPasswordCount & BadpasswordTime
+- v1.0.6 - added csv output + better display of LAPS password expiration & serviceprincipalname
+- v1.0.5 - added capability to read samaccountname list from text file (instead of typing one by one into the prompt)
+- v1.0.4 - Minor improvements in sorting countable properties & dates
+- v1.0.3 - Added better parsing for the AccountExpires attribute
+- v1.0.2 - Added multi-Domain support, and check for AD module for live domain query.
+- v1.0.1 - Added offline DB support (Updated for OSDFCon 2021 "I know what your AD did last summer!.." talk)
 
 <#
-## Examples on how to create custom accountnames.txt file(s)
-# Create an accounts file that will query changes for all administrative users (adminCount=1)
+## Example code on how to create a custom accountnames.txt file to query changes for a specific set of accounts
+# Creating an accounts file that will query changes for all administrative users (flagged as adminCount=1)
 # 1st, query the account names and save to text file
 $a = ([adsisearcher]'(&(ObjectCategory=Person)(ObjectClass=User)(admincount=1)(!(samaccountname=krbtgt)))').FindAll();
 $a.Properties.samaccountname | sort | Out-File .\accountnames.txt 
@@ -42,7 +43,58 @@ $grouped | where Name -like "cn=administrator,*"
 $grouped | where Name -like "cn=administrator,*" | select -ExpandProperty group | where AttributeName -eq "LogonCount" | Measure-Object -Property attributevalue -Sum | select -ExpandProperty Sum
 #>
 
-#$Objects = "administrator", "yossis", "DC01$"
+<#
+COMMENT ON WELL-KNOWN SIDs -->
+Dynamic groups and/or Foreign security principals, as well as some built-in groups with unicode names - might need to be queried using their SID (Security Identifier).
+e.g. 'Authenticated Users' - which is sid S-1-5-11, should be inputted as S-1-5-11, and NOT as authenticated users. otherwise the query will Not find the account in Active Directory.
+
+Here is a list of well-known SIDs that should generate replication metadata output without error (as Foreign Security Principals and/or dynamic groups|built-in):
+SID	Account Name	Description
+S-1-5-4	Interactive	Represents users who are logged in interactively.
+S-1-5-9	Enterprise Domain Controllers	Represents all domain controllers in an enterprise (forest).
+S-1-5-11	Authenticated Users	Represents all users who have authenticated successfully, regardless of the authentication method or domain.
+S-1-5-17	This Organization	Represents all users from the same organization in a federated trust scenario.
+S-1-5-32-544	Administrators	Represents the local Administrators group.
+S-1-5-32-545	Users	Represents the local Users group, which includes all authenticated users by default.
+S-1-5-32-546	Guests	Represents the local Guests group.
+S-1-5-32-551	Backup Operators	Represents the Backup Operators group, which has permissions to back up and restore files regardless of permissions.
+
+Other well-Known SIDs that might generate some replication metadata output, yet with errors:
+SID	Account Name	Description
+S-1-5-7	Anonymous Logon	Represents users who are not authenticated, including unauthenticated guest users.
+S-1-5-32-573	Terminal Server Users	Represents users who have logged on to a terminal server.
+S-1-5-32-547	Power Users	Represents the Power Users group, which historically had elevated privileges but is now deprecated in modern Windows versions.
+S-1-5-14	Remote Interactive Logon	Represents users who log on interactively through a remote desktop session.
+
+Additional well-Known SIDs that shouldn't generate replication metadata, and are locally used:
+S-1-5-18	SYSTEM	Represents the Local System account, used by Windows for system-level services.
+S-1-5-19	Local Service	Represents the Local Service account, a built-in service account with limited privileges.
+S-1-5-20	Network Service	Represents the Network Service account, used for services requiring network access with minimal privileges.
+S-1-5-6	Service	Represents all service accounts.
+S-1-5-2	Network	Represents users who log on through a network connection (e.g., accessing a shared folder).
+S-1-5-8	Proxy	Represents a proxy service account.
+S-1-5-12	Restricted Code	Represents processes running with restricted tokens.
+S-1-5-3	Batch	Represents accounts that log on using batch processing, such as scheduled tasks.
+
+As for known groups which may contain Unicode characters in the group name - you can query them by their SID instead -> 
+Enterprise Domain Controllers	S-1-5-9
+Administrators			S-1-5-32-544
+Account Operators		S-1-5-32-548
+Server Operators		S-1-5-32-549
+Print Operators			S-1-5-32-550
+Backup Operators		S-1-5-32-551
+Replicators			S-1-5-32-552
+Event Log Readers		S-1-5-32-573
+Access Control Assistance Operators	S-1-5-32-579
+Certificate Service DCOM Access		S-1-5-32-574
+Distributed COM Users			S-1-5-32-562
+Hyper-V Administrators			S-1-5-32-578
+Remote Management Users			S-1-5-32-580
+Incoming Forest Trust Builders		S-1-5-32-557
+Cryptographic Operators			S-1-5-32-569
+#>
+
+# Initialize empty array for object(s)
 $Objects = @();
 
 # Check if text file with account names exist, and if true - read it and use it
@@ -393,7 +445,15 @@ else
         $account = $_; 
         $ReplMetadata += $RespondingDCs | foreach { 
             $DC = $_;
-            $ReplOutput = Get-ADReplicationAttributeMetadata -Object $((Get-ADObject -Server $Domain -Filter {samaccountname -eq $account} -IncludeDeletedObjects).distinguishedname) -ShowAllLinkedValues -Server $DC;
+            # check if NT AUTHORITY SID was inputted, or other domain account
+            if ($account.StartsWith("S-1-5-")) {
+                $ReplOutput = Get-ADReplicationAttributeMetadata -ErrorAction SilentlyContinue -Object $((Get-ADObject -Server $Domain -Filter {objectSID -eq $account} -IncludeDeletedObjects).distinguishedname) -ShowAllLinkedValues -Server $DC;
+            }
+            else # non-SID account
+                {
+                $ReplOutput = Get-ADReplicationAttributeMetadata -Object $((Get-ADObject -Server $Domain -Filter {samaccountname -eq $account} -IncludeDeletedObjects).distinguishedname) -ShowAllLinkedValues -Server $DC;
+            }
+
             $ReplOutput | Write-Output;
 
             # get other properties, from Non-Replicated attributes
@@ -402,10 +462,28 @@ else
             switch ($ObjCategory) {
             {$_.StartsWith("CN=Computer")} {$Obj = Get-ADComputer $account -Properties logoncount,lastlogon,badpwdcount,badpasswordtime -Server $DC; [boolean]$IsGroup = $false}
             {$_.StartsWith("CN=Person")} {$Obj = Get-ADUser $account -Properties logoncount,lastlogon,badpwdcount,badpasswordtime -Server $DC; [boolean]$IsGroup = $false}
-            {$_.StartsWith("CN=Group")} {$Obj = Get-ADGroup $account -Server $DC; [boolean]$IsGroup = $true}
+            {$_.StartsWith("CN=Group")} {$Obj = Get-ADGroup $account -Server $DC -ErrorAction SilentlyContinue; [boolean]$IsGroup = $true}
+            {$_.StartsWith("CN=Foreign-Security-Principal")} {$obj = Get-ADObject -Filter {objectsid -eq $account} -Server $DC -Properties * -ErrorAction SilentlyContinue; [boolean]$IsGroup = $true}
             }
 
             $DN = $Obj.distinguishedname;
+
+            # Add well-known account name for SIDs + Lay the 'foundation' to add more relevant properties for well-known SID accounts in the future, if needed
+            if ($account.StartsWith("S-1-5-")) {
+                $SidObj = New-Object System.Security.Principal.SecurityIdentifier($account);
+                $WellKnownAccountName = $SidObj.Translate([System.Security.Principal.NTAccount]).Value;
+                
+                $SidAccountProperties = New-Object psobject;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name LastOriginatingChangeTime -Value 'N/A (Well-Known SID)' -Force;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name AttributeName -Value "Well-Known-AccountName" -Force;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name AttributeValue -Value $WellKnownAccountName -Force;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name version -Value 'N/A' -Force;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name Object -Value $DN -Force;
+                Add-Member -InputObject $SidAccountProperties -MemberType NoteProperty -Name Server -Value $DC -Force;
+
+                $SidAccountProperties | Write-Output;
+                Clear-Variable SidAccountProperties                
+            }
 
             # get values of non-replicated attributes per this DC
             if (!$IsGroup) {
